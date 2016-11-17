d59190a97aa6259fcf2b43eb5ec9dbfa7c5f8adf
Morgan <morgan.bohn@unistra.fr>
2016-11-17 14:47:43 +0100

    correct tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,6 @@
 from unittest import TestCase, main
 from element_maker.utils import XMLElement
 from lxml import etree as E
-from lxml import objectify
 
 
 class UtilsTest(TestCase):
@@ -26,12 +25,11 @@ class UtilsTest(TestCase):
         self.namespace = "fr:gouv:culture:archivesdefrance:seda:v1.0"
 
     def test_xmlify(self):
-        anp_element = XMLElement(namespace=self.namespace, **self.structure)()
-        objectify.deannotate(anp_element, cleanup_namespaces=True)
-        self.assertEqual(E.tostring(anp_element),
+        xml_element = XMLElement(namespace=self.namespace, **self.structure)()
+        self.assertEqual(E.tostring(xml_element),
             '<ArchiveTransferRequest xmlns="fr:gouv:culture:archivesdefrance:seda:v1.0" stuff="me"><Comment bli="blo">bla</Comment><Date>2014-05-12T00:00:00Z</Date></ArchiveTransferRequest>'.encode('utf-8'))
-        self.assertEqual(anp_element.attrib, {'stuff': "me"})
-        self.assertEqual(anp_element.Comment.attrib, {'bli': "blo"})
+        self.assertEqual(xml_element.attrib, {'stuff': "me"})
+        self.assertEqual(xml_element.Comment.attrib, {'bli': "blo"})
 
 
 
